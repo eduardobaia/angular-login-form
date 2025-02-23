e23391c2db0142c187dee031dce790087b6575bd
Eduardo Baia <eduardo_baia@msn.com>
2025-02-23 11:52:03 +0000

    add validation

--- login-app/src/app/components/default-login-layout/default-login-layout.component.ts
+++ login-app/src/app/components/default-login-layout/default-login-layout.component.ts
@@ -10,8 +10,12 @@ export class DefaultLoginLayoutComponent {
   @Input() title:string = "";
   @Input() primaryBtnText:string = "";
   @Input() secondaryBtnText:string = "";
+  @Input() disablePrimaryBtn: boolean =true;
   @Output("submit") onSubmit = new EventEmitter();
   @Output("navigate") onNavigate = new EventEmitter();
+
+
+
 
   submit() {
     this.onSubmit.emit();
